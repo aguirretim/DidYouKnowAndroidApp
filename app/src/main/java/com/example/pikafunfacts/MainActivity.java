8e941d66782ada694f25946619e4c078bee1b53f
aguirretim <aguirretim@ymail.com>
2020-03-28 10:12:57 -0700

    Finished version of a basic Click to show facts app

---
+++ app/src/main/java/com/example/pikafunfacts/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.pikafunfacts;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+
+    // Declare our View variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private ConstraintLayout constraintlayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assigns the Views from the layout file to the corresponding variables.
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        constraintlayout = findViewById(R.id.constraintlayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = factBook.getFact();
+                int color = colorWheel.getColor();
+                //  getFact();
+                factTextView.setText(fact);
+                constraintlayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+}
